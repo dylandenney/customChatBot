0f3769cf106be14cbfc822e4d07a7b55cb39d39c
dylandenney <dylandenney@gmail.com>
2023-05-12 13:03:18 -0400

    adding files taking out api key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import gradio as gr
 import sys
 import os
 
-os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '',
 
 def construct_index(directory_path):
     max_input_size = 4096
